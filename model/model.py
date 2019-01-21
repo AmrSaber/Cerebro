67969db29c1f52fccd7b55a5b31832467fd48568
Amr Saber <amr.m.saber.mail@gmail.com>
2019-01-21 03:59:26 +0200

    Edited the model     - now it's the same as the paper, but without normalization or features

--- model/model.py
+++ model/model.py
@@ -4,26 +4,35 @@ import keras
 from keras.layers import Input, Conv2D, MaxPooling2D, Flatten, Dense, BatchNormalization
 from keras.models import Model
 
+
 def create_model():
+
+	conv_activation = 'relu'
+	dense_activation = 'relu'
+
+	# first dimension of shape was None
 	input_image = Input(batch_shape=(None, 48, 48, 1), dtype='float32', name='input_image')
 
-	x = Conv2D(filters=64, kernel_size=(5, 5))(input_image)
-	x = MaxPooling2D(pool_size=(2, 2), data_format="channels_last")(x)
-	#x = BatchNormalization(axis=-1)
+	x = Conv2D(filters=64, kernel_size=(5, 5), padding='same', activation=conv_activation)(input_image)
+	x = MaxPooling2D(pool_size=(2, 2), strides=2, data_format="channels_last")(x)
+	x = BatchNormalization(axis=-1)(x)
 
-	x = Conv2D(filters=96, kernel_size=(5, 5))(x)
-	x = MaxPooling2D(pool_size=(2, 2), data_format="channels_last")(x)
-	#x = BatchNormalization(axis=-1)
+	x = Conv2D(filters=96, kernel_size=(5, 5), padding='same', activation=conv_activation)(x)
+	x = MaxPooling2D(pool_size=(2, 2), strides=2, data_format="channels_last")(x)
+	x = BatchNormalization(axis=-1)(x)
 
-	x = Conv2D(filters=256, kernel_size=(5, 5))(x)
-	#x = MaxPooling2D(pool_size=(2, 2), data_format="channels_last")(x)
+	x = Conv2D(filters=256, kernel_size=(5, 5), padding='same', activation=conv_activation)(x)
+	x = MaxPooling2D(pool_size=(2, 2), strides=2, data_format="channels_last")(x)
+	# x = BatchNormalization(axis=-1)(x)
 
-	x = Conv2D(filters=256, kernel_size=(5, 5))(x)
+	x = Conv2D(filters=256, kernel_size=(5, 5), padding='same', activation=conv_activation)(x)
+	# x = BatchNormalization(axis=-1)(x)
+
 	x = Flatten()(x)
 
-	x = Dense(units=2048, activation='relu')(x)
+	x = Dense(units=2048, activation=dense_activation)(x)
 
-	output = Dense(units=7, activation='softmax')(x)
+	output = Dense(units=5, activation='softmax')(x)
 
 	model = Model(inputs=[input_image], outputs=[output])
 	model.compile(optimizer='adam', loss='mean_squared_error', metrics=['accuracy'])
--- model/reader.py
+++ model/reader.py
@@ -2,6 +2,10 @@
 
 import numpy as np
 
+# old_emotions = ["Angry", "Disgust", "Fear", "Happy", "Sad", "Surprise", "Neutral"]
+# new_emotions = ["Fear", "Neutral", "Satisfied", "Surprise", "Unsatisfied"]
+emotions_map = [4, 4, 0, 2, 4, 3, 1]
+
 def read_data(file_name = 'dataset/fer2013.csv', limit = 1000):
 	x_train, y_train, x_test, y_test = [], [], [], []
 	with open(file_name) as file:
@@ -11,7 +15,9 @@ def read_data(file_name = 'dataset/fer2013.csv', limit = 1000):
 			emotion = int(emotion)
 			pxls = [int(e) for e in pxls.split(' ')]
 			image = np.reshape(pxls, (48, 48, 1))
-			
+
+			emotion = emotions_map[emotion]
+
 			if 'training' in usage.lower():
 				x_train.append(image)
 				y_train.append(emotion)
