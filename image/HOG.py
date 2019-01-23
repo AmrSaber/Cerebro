97b3e8b70688f3ba4dce9af37edd8e4f8e993b58
aim97 <mohamedmaim97@gmail.com>
2019-01-23 15:30:22 +0200

    - bug fix for image resize:   bug : when the face detection makes a mistake like adding a detected face         of size zero, the resize code causes an error.   fix : code was updated to check for such situations so that such faces 	arne't including in detections to begin with.
    - add function is_one_face(img) in FaceDetector that checks if the image
      contains only one face

--- image/HOG.py
+++ image/HOG.py
@@ -3,18 +3,22 @@ from skimage.feature import hog
 import matplotlib.pyplot as plt
 import cv2
 
+
 def sk_get_hog(img):
-	features, hog_image = hog(img,
+	features, hog_image = hog(
+		img,
 		orientations=9,
 		pixels_per_cell=(8, 8),
 		cells_per_block=(3, 3),
 		visualize=True,
 		transform_sqrt=False,
 		feature_vector=True,
-		multichannel=True)
+		multichannel=True
+	)
 	io.imshow(hog_image)
 	plt.show()
 	return features
+
 
 def cv_get_hog(img):
 	hog = cv2.HOGDescriptor()
