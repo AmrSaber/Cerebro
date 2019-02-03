c596189839d90cf49e3ef649b05f8c837b7443ae
Amr Saber <amr.m.saber.mail@gmail.com>
2019-02-03 15:45:32 +0200

    added branch image-dev

--- image/face_detector/dlib_FaceDetector.py
+++ image/face_detector/dlib_FaceDetector.py
@@ -2,7 +2,38 @@ from imutils import face_utils
 import cv2
 import dlib
 
+__dlib_detector = dlib.get_frontal_face_detector()
 
+# public interface
+def get_faces(img):
+	rects = __dlib_detector(img, 2)
+	res = []
+	for rect in rects:
+		face = _extract_face(img, rect)
+		face = _enhance_face(face)
+		p1 = rect.tl_corner()
+		p2 = rect.br_corner()
+		box = [(p1.x, p1.y), (p2.x, p2.y)]
+		res.append((face, box))
+	return res
+
+def is_one_face(img):
+	return len(__dlib_detector(img, 2)) == 1
+
+# private methods
+def _extract_face(img, rect):
+	p1 = rect.tl_corner()
+	p2 = rect.br_corner()
+	face = img[p1.y:p2.y, p1.x:p2.x]
+	return face
+
+def _enhance_face(face):
+	face = cv2.resize(face, (48, 48))
+	face = cv2.cvtColor(face, cv2.COLOR_RGB2GRAY)
+	return face
+
+########################################################
+"""
 class FaceDetector:
 	detector = dlib.get_frontal_face_detector()
 	predictor = dlib.shape_predictor("../saved-models/face-landmarks/shape_predictor_68_face_landmarks.dat")
@@ -52,11 +83,11 @@ class FaceDetector:
 			faces[i] = cv2.resize(faces[i], (48, 48))
 			faces[i] = cv2.cvtColor(faces[i], cv2.COLOR_RGB2GRAY)
 		return faces
-
+"""
 
 if __name__ == '__main__':
-	im = cv2.imread("examples/FacesOfDarbya.jpg")
-	in_image_faces = FaceDetector.get_faces(im)
+	im = cv2.imread("index.jpeg")
+	in_image_faces = get_faces(im)
 	some_face = in_image_faces[0][0]
 	cv2.imshow("face", some_face)
 	cv2.waitKey(0)
---
+++ image/face_detector/haar.py
@@ -0,0 +1,3 @@
+from cascade_FaceDetector import FaceDetector
+
+__haar_facedetector = 
