d1d4aeb709a412904f2f63ae5083317907ea3543
ManarArabi <manar.araby.ma@gmail.com>
2019-02-13 19:22:49 +0200

    errors are fixed

--- process_image.py
+++ process_image.py
@@ -2,9 +2,10 @@ from model import predict
 from reader import emotions_map
 
 
-def extract_faces_emotion(image, detector_type):
+def extract_faces_emotion(image, detector_type = 'dlib'):
     """
     detector_type >> ('dlib, haar, lbp')
+    default >> dlib
     each item has [face_image, corner_coordinate, emotion]
     faces of size 48*48 , gray scale
     corners coordinates as ()
@@ -33,28 +34,44 @@ def extract_faces_emotion(image, detector_type):
         item.clear
     return items
 
-def mark_faces_emotions(image, detector_type):
+def mark_faces_emotions(image, detector_type = 'dlib'):
+    """
+    default detector >> dlib
+    """
     font = cv2.FONT_HERSHEY_SIMPLEX
-    font_scale = 1
+    font_scale = 2
     font_color = (77, 121, 255)
-    line_type = 2
+    offest_x = 2
+    offest_y = 5
 
     extracted_faces_emotions = extract_faces_emotion(image, detector_type)
-    for i in range(len(extracted_faces_emotions)):
-        cv2.rectangle(
-            image,
-            extracted_faces_emotions[i][1][0],
-            extracted_faces_emotions[i][1][1],
-            (66,206,244),
-            2)
-        cv2.putText(img,
-            extracted_faces_emotions[i][2],
-            extracted_faces_emotions[i][1][0]+2,
-            font,
-            font_scale,
-            font_color,
-            line_type)
+    """
+    extracted_faces_emotions[i] >>
+                item (face, corners(topright,bottomleft), emotion)
+    extracted_faces_emotions[i][1] >> corners
+    extracted_faces_emotions[i][1][0] >> topright
+    extracted_faces_emotions[i][1][0][0] >> toprightX
+    extracted_faces_emotions[i][1][0][1] >> toprightY
+    extracted_faces_emotions[i][2] >> emotion
+    """
 
-    cv2.imshow("detected emotions",image)
-    cv2.waitKey(0)
+    for i in range(len(extracted_faces_emotions)):
+        tmp = (extracted_faces_emotions[i][1][0][0]-offest_x,
+               extracted_faces_emotions[i][1][0][1]-offest_y)
+
+        image = cv2.rectangle(image,
+                              extracted_faces_emotions[i][1][0],
+                              extracted_faces_emotions[i][1][1],
+                              (66,206,244),
+                              1)
+        image = cv2.putText(image,
+                            extracted_faces_emotions[i][2],
+                            tmp,
+                            font,
+                            font_scale,
+                            text_color,
+                            2)
+    #to display
+    #cv2.imshow("detected emotions",image)
+    #cv2.waitKey(0)
     return image
--- testing.py
+++ testing.py
@@ -2,7 +2,7 @@
 
 import cv2
 
-def extract_faces_emotion(image, detector_type):
+def extract_faces_emotion(image, detector_type  = 'dlib'):
     if detector_type == 'dlib':
         from image.face_detector import dlib as detector
     elif detector_type =='haar':
@@ -16,24 +16,30 @@ def extract_faces_emotion(image, detector_type):
     return faces
 
 if __name__ == '__main__':
-    im = cv2.imread("y.jpg")
-    x = extract_faces_emotion(im,'haar')
+    im = cv2.imread("b.jpg")
+    x = extract_faces_emotion(im)
 
-    font = cv2.FONT_HERSHEY_SIMPLEX
-    font_scale = 1
-    font_color = (77, 121, 255)
+    font = cv2.FONT_HERSHEY_PLAIN
+    font_scale = 2
+    box_color = (0, 255, 255)
+    text_color = (0, 0, 0)
     emotion = []
     for i in range(len(x)):
         emotion.append('face')
     for i in range(len(x)):
-        cv2.rectangle(im,x[i][1][0],x[i][1][1],(66,206,244),2)
-        cv2.putText(im,
+        im = cv2.rectangle(im,x[i][1][0],x[i][1][1],box_color,1)
+        y = (x[i][1][0][0]-2, x[i][1][0][1]-5)
+        im = cv2.putText(im,
             emotion[i],
-            x[i][1][0]+(0, 2),
+            y,
             font,
             font_scale,
-            font_color,
-            cv2.LINE_AA)
-
+            text_color,
+            2)
     cv2.imshow("detected faces",im)
     cv2.waitKey(0)
+    """
+    x[i] >> item (face, corners(topright,bottomleft), emotion)
+    x[i][1] >> corners
+    x[i][1][0] >> topright
+    """
