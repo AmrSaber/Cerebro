4171b72cb3278b130d929871a70d9e4f797f1cd7
ManarArabi <manar.araby.ma@gmail.com>
2019-03-08 18:37:33 +0200

    initial commit

---
+++ interface/process_video.py
@@ -0,0 +1,50 @@
+import cv2
+import numpy as np
+#import sys;
+#sys.path.insert(1, '../model')
+#sys.path.insert(1, '../image')
+#sys.path.insert(1, '../saved-models/emotions_model.f5')
+#sys.path.insert(1,'../image/face_detector')
+#from face_detector import detect_dlib as detector
+"""
+def __display(img, boxes):
+    for box in boxes:
+        cv2.rectangle(img, box[0], box[1], (0, 255, 0), 1)
+    cv2.imshow("detected faces", img)
+"""
+import process_image as pi
+if __name__ == '__main__':
+
+    # Create a VideoCapture object and read from input file
+    # If the input is the camera, pass 0 instead of the video file name
+    cap = cv2.VideoCapture('x.mp4')
+    fps = 24
+    # Check if camera opened successfully
+    if (cap.isOpened()== False):
+      print("Error opening video stream or file")
+
+    # Read until video is completed
+    while(cap.isOpened()):
+      # Capture frame-by-frame
+        ret, frame = cap.read()
+        if ret == True:
+            frame = cv2.resize(frame, (800,600))
+            #x = detector.get_faces(frame)
+            #__display(frame, x)
+            #  Display the resulting frame
+            frame = pi.mark_faces_emotions(frame)
+            cv2.imshow('Frame',frame)
+
+            # Press Q on keyboard to  exit
+            if cv2.waitKey((int)(1000 / fps)) & 0xFF == ord('q'):
+                break
+
+                # Break the loop
+        else:
+            break
+
+    # When everything done, release the video capture object
+    cap.release()
+
+    # Closes all the frames
+    cv2.destroyAllWindows()
