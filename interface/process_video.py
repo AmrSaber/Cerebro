64d1070e625f9babc154c9b42fa938ab0353c59f
ManarArabi <manar.araby.ma@gmail.com>
2019-06-04 18:34:42 +0200

    audio is working, colors is fixed.-done-

--- interface/process_video.py
+++ interface/process_video.py
@@ -7,37 +7,37 @@ import numpy as np
 import process_image as pi
 from moviepy.editor import *
 
-
-def display_video(frames, video_name="video"):
-
-    for i in range(len(frames)-1):
-        cv2.imshow(video_name,frames[i])
-        if cv2.waitKey(200) & 0xFF == ord('q'):
-             break
-
-def detect_video_emotions(video_path, skip = 50):
-
-    #audio part 
+def detect_video_emotions(video_path, output_path, skip = 50):
+    """
+    skip >> detemines number of skipped frames during procerssing
+    in normal video speed skip=50 means detect emotions every 2 seconds
+    higher skip means faster processing but less accurate
+    -----------------------------------------
+    output_path >> include output name & extension
+    the output video extension should be mp4
+    """
+    #saving audio 
     video = VideoFileClip("x.mp4")
     audio = video.audio
 
     #processing frames
     vidObj = cv2.VideoCapture(video_path)
-    video_fps = vidObj.get(5)#fps
+    fps = vidObj.get(5) #fps
 
-    success = 1 # checks whether frames were extracted
+    success = 1 #checks whether frames were extracted
     real_frame_counter = 1 #to check with sampling
     sampled_frame_counter = 1 #to print frame number for user
     prev_frame_data = None
 
     img_frames = []
 
+    #getting frames
     while success:
         success, image = vidObj.read()
 
         if not success :
             break
-
+        #processing frames
         if not(real_frame_counter % skip):
             prev_frame_data = pi.extract_faces_emotions(image)
             print("process frame: ", sampled_frame_counter)
@@ -45,42 +45,14 @@ def detect_video_emotions(video_path, skip = 50):
 
         image = pi.mark_faces_emotions(image,"None", prev_frame_data)
         real_frame_counter += 1
-
+        image = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
         img_frames.append(image)
-        
-    return img_frames, audio, video_fps
-
-def save_video(img_frames, audio, video_name,fps = 25): 
-    """
     
-    output will be .avi
-    
-    fourcc = cv2.VideoWriter_fourcc(*'XVID')
-    height, width, layers = img_frames[0].shape
-    video = cv2.VideoWriter(video_name, fourcc, fps, (width,height))
-    for i in range(len(img_frames)):
-        video.write(img_frames[i])
-        
-    cv2.destroyAllWindows()
-    video.release()
-    """
+    #making output video
+    print("making output video...")
     clips = [ImageClip(m).set_duration(1/fps)
          for m in img_frames]
     concat_clip = concatenate_videoclips(clips, method="chain")
     concat_clip_edited = concat_clip.set_audio(audio)
-    concat_clip_edited.write_videofile("test.mp4", fps=fps)
-    
-
-if __name__ == '__main__':
-    
-    img_frames, audio, fps = detect_video_emotions("x.mp4")
-    save_video(img_frames, audio,"xx.avi", fps)
-
-    """
-    video2 = VideoFileClip("xx.avi")
-    edited = video2.set_audio(audio)
-    edited.write_videofile("test.mp4", fps = fps)
-    """
-    #display_video(frames)
-    
-    
+    concat_clip_edited.write_videofile("output.mp4", fps=fps)
+    return 
