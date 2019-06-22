c77bb1dc552bee97625d48b9e620d500a1cd765b
Amr Saber <amr.m.saber.mail@gmail.com>
2019-06-22 16:14:52 +0200

    Refactor stream connection

--- interface/video_stream.py
+++ interface/video_stream.py
@@ -1,41 +1,54 @@
-import cv2
-import time
-from imutils.video import VideoStream
 from threading import Thread
-from interface import process_image as pi
 from queue import Queue
+import time
 
-task_queue = Queue(1)
-result_queue = Queue()
+import cv2
+from imutils.video import VideoStream
 
-def worker( task_queue, result_queue):
-        print("running")
-        while True:
-            if not task_queue.empty():
-                result_queue.put(pi.extract_faces_emotions(task_queue.get()))
-
+from interface import process_image as pi
 
 def detect_stream_emotions(fps):
     frame_counter = 0
+    frame_data = None
     vs = cv2.VideoCapture(0)
     time.sleep(2.0)
 
+    worker_thread = WorkerThread(frame_data)
+    worker_thread.start()
+
     while True:
-        res, frame = vs.read()
-        if res:
-            frame_counter += 1
-            if not result_queue.empty():
-                frame_data = result_queue.get()
-                frame = pi.mark_faces_emotions(frame, None, frame_data)
-            cv2.imshow("Frame", frame)
-            if not (frame_counter % fps) :
-                if not task_queue.full():
-                    task_queue.put(frame)
-        if cv2.waitKey(1) & 0xFF == ord("q"):
-            break
+        success, frame = vs.read()
+        if not success: continue
+        
+        frame_counter += 1
+
+        if (frame_counter % fps) == 0:
+            worker_thread.add_task(frame)
+
+        if frame_data != None:
+            frame = pi.mark_faces_emotions(frame, None, frame_data)
+
+        cv2.imshow("Frame", frame)
+
+        if cv2.waitKey(1) & 0xFF == ord("q"): break
+    
     vs.release()
     cv2.destroyAllWindows()
+    worker_thread.kill()
 
+class WorkerThread(Thread):
+    def __init__(self, result_container):
+        self.queue = Queue()
+        self.result_container = result_container
+    
+    def run(self):
+        while True:
+            frame = self.queue.get()
+            if frame == -1: break
+            self.result_container = pi.extract_faces_emotions(frame)
+    
+    def add_task(self, frame):
+        self.queue.put(frame)
 
-mythread = Thread(target = worker, args=(task_queue, result_queue))
-mythread.start()
+    def kill(self):
+        self.queue.put(-1)
