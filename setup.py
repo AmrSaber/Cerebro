b3724e873720f2a10eb319bda51b477877f3416b
ManarArabi <manar.araby.ma@gmail.com>
2019-06-23 23:14:58 +0200

    setup.py is updated

--- setup.py
+++ setup.py
@@ -1,29 +1,17 @@
 from distutils.core import setup
+import setuptools
 setup(
-    name = 'Cerebro',
-    packages = ['Cerebro'],
+    name = 'CEREBRO1',
     version = '1.0',
     license='gpl-3.0',
     description = 'recognizing facial expressions from images, videos and real-time stream',
-    long_description = read('README.md'),
     author = 'AmrSaber, WafaaIsmail, MohamedAhmed, SalmaSayed, MohamedAref, ManarArabi',
     author_email = 'amr.m.saber.mail@gmail.com, wafaaismail595@gmail.com, mohamedmaim97@gmail.com, Salmasayed797@gmail.com, muhammad.aref224@gmail.com, manar.araby.ma@gmail.com',
     url = 'https://github.com/AmrSaber/Cerebro',
     download_url = 'https://github.com/AmrSaber/Cerebro/archive/v1.0.tar.gz',
     keywords = ['emotions', 'expressions', 'real-time stream'],
-    install_requires=[
-            'opencv-python',
-            'numpy',
-            'moviepy',
-            'ffmpeg-python',
-            'imutils',
-            'argparse',
-            'keras',
-            'pathlib',
-            'matplotlib',
-            'scikit-image',
-            'dlib',
-            ],
+    packages=setuptools.find_packages(),
+    
     classifiers=[
         'Development Status :: 5 - Production/Stable',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
         'Intended Audience :: Developers',
@@ -34,3 +22,4 @@ setup(
         'Programming Language :: Python :: 3.6',
         ],
 )
+print(setuptools.find_packages())
